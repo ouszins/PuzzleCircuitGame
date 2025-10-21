00f6c943719bdf93ce72170a4e4df59c44028bd3
WafflyBoi <jamesiebuckley@gmail.com>
2025-10-21 22:50:58 +0100

    CircuitBase
    Base of circuit, to be modified/changed

---
+++ Assets/SensorScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CircuitSensor : MonoBehaviour
+{
+    public bool isConnected = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //This checks if the Sensor is interacting to another sensor
+        if (other.CompareTag("Sensor"))
+        {
+            Debug.Log($"{name} connected to {other.name}");
+            isConnected = true;
+
+            //Telling the parent to change colour (as debug or as an interaction)
+            transform.parent.GetComponent<Renderer>().material.color = Color.green;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if it is not touching another sensor it changes the parent colour to a different one, debug/interaction purposes
+        if (other.CompareTag("Sensor"))
+        {
+            Debug.Log($"{name} disconnected from {other.name}");
+            isConnected = false;
+
+            transform.parent.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
+
