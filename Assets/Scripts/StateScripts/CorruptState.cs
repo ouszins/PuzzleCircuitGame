1b435a627482d2f487a4f855cd7ebd3803ed82cf
ouszin <sara.oliszewska65@gmail.com>
2025-10-30 14:48:48 +0000

    brand new folders. and more

--- Assets/Scripts/StateScripts/CorruptState.cs
+++ Assets/Scripts/StateScripts/CorruptState.cs
@@ -3,7 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CorruptState : MonoBehaviour
-{
+{   
+    //plan for this one is just. to have guido enter corruption
+    //over time, corruption should cause a progress bar to elapse
+    // if progress bar becomes full, game is over
+    // james will handle guido messing with the circuits
+
+    // guido should turn purple. 
+    // done by swapping materials over time
+    // rgb parameter where guido would turn purple
+
+    // if progress bar = x, then guido receives a tint/rgb value shift to x
+    // if purple == true, game over
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/StateScripts/State.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class State : MonoBehaviour
-{
-    // Start is called before the first frame update
-    public abstract State RunCurrentState();
-}
