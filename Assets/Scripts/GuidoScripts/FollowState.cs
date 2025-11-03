2d7868564d6ab68cfc9e2a8998f659b50b996eec
WafflyBoi <jamesiebuckley@gmail.com>
2025-11-03 13:16:04 +0000

    Fixed follow him to stop at certain radius

--- Assets/Scripts/GuidoScripts/FollowState.cs
+++ Assets/Scripts/GuidoScripts/FollowState.cs
@@ -7,7 +7,7 @@ public class FollowHim : State
 {
     public Transform player; //will assigne to player
     public float followRadius = 10f; // the distance he should be around the player
-
+    public float StopRadius = 10f; // the distance he should stop from the player
     public float GooberSpeed = 5f; // speed of the goober(i love autocomplete)
     public float HeightOffGround = 1.5f; // height off the ground the NPC will stay at
    
@@ -22,12 +22,16 @@ public class FollowHim : State
         }
 
         float distance = Vector3.Distance(transform.position, player.position);
-
-        if (distance > followRadius)
+        //only moves if farther than Stopdistance and within follow distance
+        if (distance >  StopRadius && distance < followRadius)
         {
             // Move towards the player
             Vector3 AimPosition = new Vector3(player.position.x, transform.position.y, player.position.z);
             Vector3 direction = (player.position - transform.position).normalized;
+
+            Vector3 targetStopPos = player.position - player.forward * StopRadius;//should calculate distance away from player to stop at
+            targetStopPos.y = transform.position.y;
+
             transform.position += direction * GooberSpeed * Time.deltaTime;
 
             transform.LookAt(player); // Make the NPC face the player while moving
@@ -41,6 +45,8 @@ public class FollowHim : State
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(player.position, followRadius);// something similar could be used so sense if circuits are completed?
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(player.position, StopRadius); // visualise the  stop radius
     }
 
 }
