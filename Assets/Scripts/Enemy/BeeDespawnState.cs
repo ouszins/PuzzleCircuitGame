424d6a12569a9b0d92b537f2e8731a4ececd6a35
kiwinoodles <118855558+kiwinoodles@users.noreply.github.com>
2025-11-04 20:26:13 +0000

    just got rid of the comments. will split into branches

--- Assets/Scripts/Enemy/BeeDespawnState.cs
+++ Assets/Scripts/Enemy/BeeDespawnState.cs
@@ -1,24 +1,24 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class BeeDespawnState : BeeState
-//{
-//    public override BeeDespawnState Run()
-//    {
-//        return this;
-//    }
+public class BeeDespawnState : BeeState
+{
+    public override BeeDespawnState Run()
+    {
+        return this;
+    }
 
-//    public override BeeDespawnState Run(Vector3 enemyDir, float enemySpeed, bool isGrounded, bool isPlayerContact, bool isEnemyContact, bool isPlayerSeen)
-//    {
+    public override BeeDespawnState Run(Vector3 enemyDir, float enemySpeed, bool isGrounded, bool isPlayerContact, bool isEnemyContact, bool isPlayerSeen)
+    {
 
-//        //Focus on Despawning first
-//        DespawnBee();
-//        return this;
-//    }
+        //Focus on Despawning first
+        DespawnBee();
+        return this;
+    }
 
-//    public void DespawnBee()
-//    {
-//        Destroy(transform.parent.parent.gameObject);
-//    }
-//}
+    public void DespawnBee()
+    {
+        Destroy(transform.parent.parent.gameObject);
+    }
+}
