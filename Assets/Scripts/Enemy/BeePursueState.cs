88ebaeb4003e05a84eed8669cc7578efba8e4583
kiwinoodles <118855558+kiwinoodles@users.noreply.github.com>
2025-11-04 22:53:09 +0000

    changed some titles, fixed a recurring issue

--- Assets/Scripts/Enemy/BeePursueState.cs
+++ Assets/Scripts/Enemy/BeePursueState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PursuePlayer : BeeState
+public class BeePursuePlayer : BeeState
 {
     public override BeeState Run()
     {
