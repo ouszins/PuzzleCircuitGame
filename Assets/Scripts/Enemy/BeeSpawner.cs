9b157944c081aa2447086c1643e91aaeadba4f99
ouszin <sara.oliszewska65@gmail.com>
2025-11-05 12:18:50 +0000

    okay i saved some stuff. comment this script out for now, its going to clash till i get home.

--- Assets/Scripts/Enemy/BeeSpawner.cs
+++ Assets/Scripts/Enemy/BeeSpawner.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BeeSpawner : MonoBehaviour
 {
+
+    [SerializeField] private GameObject beePrefab;
+
+    [SerializeField] private float beeInterval = 3.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnBee(beeInterval, beePrefab));
+        //tutorial for some reason inculded a big version of the bee. 
+        //we do not *have* a big version of the enemy bee.
+
+
+        //small enemy bee. (insert jerma beatbox)
     }
 
     // Update is called once per frame
-    void Update()
+    /*  commenting out the following for now. gonna work on it more later.
+    void IEnumerator spawnBee(float interval, GameObject enemy)
     {
-        
+        yield return new WaitForSeconds(beeInterval);
+        GameObject newBee = Instantiate(enemy, new Vector3(Random.Range(-5f, 5), Random.Range(-6f, 6f), 0), Quaternion.identity);
+        StartCoroutine(spawnBee(beeInterval, enemy));
     }
+    */
 }
